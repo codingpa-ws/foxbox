6724fdf92a4b9e6ba324ac14d5da4be40ef4e656
Kev <kevslashnull@gmail.com>
2024-09-14 22:00:32 +0200

    Make slirp.Start easier to read

--- internal/slirp/slirp.go
+++ internal/slirp/slirp.go
@@ -7,7 +7,13 @@ import (
 )
 
 func Start(pid int) (*exec.Cmd, error) {
-	cmd := exec.Command("slirp4netns", "--configure", "--mtu=65520", "--disable-host-loopback", strconv.Itoa(pid), "top0")
+	cmd := exec.Command(
+		"slirp4netns",
+		"--configure",
+		"--mtu=65520",
+		"--disable-host-loopback", strconv.Itoa(pid),
+		"top0",
+	)
 
 	err := cmd.Start()
 	if err != nil {
